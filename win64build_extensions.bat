bfb0f1bef739d23df30b9f728d166327029082de
schiffma <lorenz.schiffmann@gmail.com>
2021-06-20 23:14:36 +0200

    Version 1.0

--- win64build_extensions.bat
+++ win64build_extensions.bat
@@ -3,7 +3,7 @@ SET EXT_PATH=C:\Users\schiffma\Documents\GitHub\generate_series
 :: Set path variables
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-CD %EXT_DIR%
+CD %EXT_PATH%
 
 :: Build extensions
 cl /MD series.c -link -dll -out:series_64.dll
