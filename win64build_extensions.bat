ff08b83ecc6cc1be22b8409bdce2ad2c53f5f1e8
schiffma <lorenz.schiffmann@gmail.com>
2021-06-20 21:42:21 +0200

    Update win64build_extensions.bat

--- win64build_extensions.bat
+++ win64build_extensions.bat
@@ -1,22 +1,12 @@
-:: Destination path - specify where to move package after build
-SET DEST_PATH=C:\Users\schiffma\Documents\GitHub\generate_series
-MKDIR "%DEST_PATH%"
-
-SET SQLITE_DIR=C:\Users\schiffma\Documents\GitHub\generate_series
-
-
-:: WIN64 SQLITE BUILD PROCEDURE
-
+:: Extension path - specify where this extension will be built.
+SET EXT_PATH=C:\Users\schiffma\Documents\GitHub\generate_series
 :: Set path variables
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-:: Build SQLite x64
-CD %SQLITE_DIR%
-:: cl sqlite3.c -DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_STAT4 -DSQLITE_SOUNDEX -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_GEOPOLY -DSQLITE_ENABLE_RTREE -DSQLITE_API=__declspec(dllexport) -link -dll -out:sqlite3.dll
+CD %EXT_DIR%
 
 :: Build extensions
-
-cl /MD series.c -link -dll -out:series64.dll
+cl /MD generate_series.c -link -dll -out:generate_series_64.dll
 
 pause
 
